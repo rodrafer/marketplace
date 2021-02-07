fdfbe9a0588ec170b3cd034bb8f70660fb78f56b
Rodrigo Fernandez Frittelli <rodrigoff95@gmail.com>
2021-02-07 16:43:33 -0300

    Adding Query and Item endpoints. Including eslint

--- marketplace-backend/src/server.js
+++ marketplace-backend/src/server.js
@@ -1,6 +1,92 @@
 import express from 'express';
+const fetch = require('node-fetch');
 
 const app = express();
 const PORT = 8000;
+
+app.get('/api/items', async (req, res) => {
+    const q = req.query.q;
+    let rawItems;
+    let categoryFilter;
+    let categories = [];
+    let formatedItems = [];
+
+    await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${q}`)
+        .then(response => response.json())
+        .then(data => {
+            rawItems = data.results.slice(0, 4);
+            categoryFilter = data.filters.find(filter => filter.id === 'category');
+        });
+
+    rawItems.forEach(rawItem => {
+        const formatedItem = {
+            id: rawItem.id,
+            title: rawItem.title,
+            price: {
+                currency: rawItem.prices.prices[0].currency_id,
+                amount: rawItem.prices.prices[0].amount,
+                decimals: 0
+            },
+            picture: rawItem.thumbnail,
+            condition: rawItem.condition,
+            free_shipping: rawItem.shipping.free_shipping
+        };
+        formatedItems.push(formatedItem);
+    });
+
+    categoryFilter.values[0].path_from_root.forEach(path => {
+        categories.push(path.name);
+    });
+
+    const results = {
+        author: {
+            name: 'Rodrigo',
+            lastname: 'Fernandez'
+        },
+        categories: categories,
+        items: formatedItems
+    };
+    
+    res.status(200).send(results);
+});
+
+app.get('/api/items/:id', async (req, res) => {
+    const id = req.params.id;
+    let itemDetails;
+    let itemDescription;
+
+    await fetch(`https://api.mercadolibre.com/items/${id}`)
+        .then(response => response.json())
+        .then(data => itemDetails = data);
+
+    await fetch(`https://api.mercadolibre.com/items/${id}/description`)
+        .then(response => response.json())
+        .then(data => itemDescription = data);
+
+    const formatedItem = {
+        id: itemDetails.id,
+        title: itemDetails.title,
+        price: {
+            currency: itemDetails.currency_id,
+            amount: itemDetails.price,
+            decimals: 2
+        },
+        picture: itemDetails.thumbnail,
+        condition: itemDetails.condition,
+        free_shipping: itemDetails.shipping.free_shipping,
+        sold_quantity: itemDetails.sold_quantity,
+        description: itemDescription.plain_text
+    };
+    
+    const results = {
+        author: {
+            name: 'Rodrigo',
+            lastname: 'Fernandez'
+        },
+        item: formatedItem
+    };
+
+    res.status(200).send(results);
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
